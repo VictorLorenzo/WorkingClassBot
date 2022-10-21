bb2783887a7ad6a7f477642f755cf7ec6cf3c7a6
Victor Lorenzo Pereira Torres <victor.torres@itriad.org.br>
2022-10-21 14:56:06 -0400

    feat: commands paste, integrated with index

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 require("dotenv").config();
-
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const token = process.env.TOKEN_DISCORD; //Token that you saved in step 5 of this tutorial
-const {Client, GatewayIntentBits } = require("discord.js");
-const client = new Client({
-    intents:[
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages
-    ],
-    
-});
-client.on("ready", () =>{
-    console.log("The AI bot is online"); //message when bot is online
-});
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.on("message", (message) => {
-    if (message.content.substring(0, 1) === "!") {
-        message.channel.send("Hello from AI bot"); //reply if message has "!" as first character
-    }
-});
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	// Set a new item in the Collection with the key as the command name and the value as the exported module
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
+
 
 client.login(token);
