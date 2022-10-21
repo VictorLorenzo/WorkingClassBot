2857f5f0a697c7e20d8ca997d408e732393a2f20
Victor Lorenzo Pereira Torres <victor.torres@itriad.org.br>
2022-10-21 11:47:08 -0400

    fix: Intents update

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 require("dotenv").config();
 
 const token = process.env.TOKEN_DISCORD; //Token that you saved in step 5 of this tutorial
-const {Client, GatewayIntentBits} = require("discord.js");
+const {Client, GatewayIntentBits } = require("discord.js");
 const client = new Client({
     intents:[
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages
     ],
+    
 });
 client.on("ready", () =>{
     console.log("The AI bot is online"); //message when bot is online
 });
+
+client.on("message", (message) => {
+    if (message.content.substring(0, 1) === "!") {
+        message.channel.send("Hello from AI bot"); //reply if message has "!" as first character
+    }
+});
+
 client.login(token);
